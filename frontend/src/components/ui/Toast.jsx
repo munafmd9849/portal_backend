bf48a47240c565ef49341e1a9c16a78c5831a290
Esha Bajaj <eshabajaj1626@gmail.com>
2026-07-13 00:50:12 +0530

    toast message fix

--- frontend/src/components/ui/Toast.jsx
+++ frontend/src/components/ui/Toast.jsx
@@ -114,7 +114,7 @@ const Toast = ({ id, type, title, message, onClose, duration = 5000 }) => {
 // Toast Container
 const ToastContainer = ({ toasts, removeToast }) => {
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-[10050] w-[min(28rem,calc(100vw-2rem))] space-y-2 flex flex-col-reverse">
+    <div className="fixed top-4 right-4 z-[10050] w-[min(28rem,calc(100vw-2rem))] space-y-2 flex flex-col pointer-events-none [&>*]:pointer-events-auto">
       {toasts.map((toast, index) => (
         <div
           key={toast.id}
